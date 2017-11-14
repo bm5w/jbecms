d7889f5a7ae66d918e7f9f8b045e96881c9ea163
Erich Cline <erichcline@yahoo.com>
2017-11-13 16:25:57 -0800

    Text Box Input

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TextInput extends React.Component {
+    constructor (props) {
+        super(props);
+
+    }
+
+    render() {
+        
+        return (
+            <div>
+                text input
+            </div>
+        )
+    }
+}
+
+export default TextInput;
