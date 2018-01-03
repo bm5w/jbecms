fedadcca109186c37f31ba7c758f257e5eebb53f
Jason Berney <jasonrberney@gmail.com>
2018-01-03 14:40:52 -0800

    Old changes to JBECMS.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,20 +4,20 @@ class Item extends React.Component {
     constructor (props) {
         super(props);
 
-//         this.state = {
-//             purchased: props.purchased ? props.purchased : false,
-//             name: props.name ? props.name : '',
-//             price: props.price ? props.price : 0,
-//             purchasedOnChange: props.purchasedOnChange,
-//             index: props.index
-//         };
+        this.state = {
+            purchased: props.purchased ? props.purchased : false,
+            name: props.name ? props.name : '',
+            price: props.price ? props.price : 0,
+        }
 
         this.onChange = this.onChange.bind(this);
+
     }
 
     onChange(event) {
         //this.setState({...this.state, purchased: !props.purchased})
-        this.props.purchasedOnChange(event, this.props.index);
+        this.setState({purchased: event.target.checked})
+        
     }
 
     render() {
@@ -25,9 +25,9 @@ class Item extends React.Component {
         return (
             <div>
                 <li>
-                    <input type='checkbox' checked={this.props.purchased} onChange={this.onChange}/>
-                    <label>{this.props.name}</label>
-                    <label>${this.props.price}</label>
+                    <input type='checkbox' checked={this.state.purchased} onChange={this.onChange}/>
+                    <label>{this.state.name}</label>
+                    <label>${this.state.price}</label>
                 </li>
             </div>
         )
